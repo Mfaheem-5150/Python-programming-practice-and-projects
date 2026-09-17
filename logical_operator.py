8bbdde6d7ca2789da4fe574082a2012589bf0965
Muhammad Faheem <muhammadfaheem9986@gmail.com>
2026-09-17 12:27:05 +0500

    Add files via upload

---
+++ logical_operator.py
@@ -0,0 +1,19 @@
+# x=True
+# y=False
+# logical operator "and"
+
+print("True and True:",True and True)
+print("True and False:",True and False)
+print("False and True:",False and True)
+print("False and False:",False and False)
+
+# logical operator "or"
+
+print("True or True:",True or True)
+print("True or False:",True or False)
+print("False or True:",False or True)
+print("False or False:",False or False)
+
+# logical operator "not"
+print("not True:",not True)
+print("not False:",not False)
