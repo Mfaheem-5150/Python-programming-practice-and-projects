16ab7ecc47961dfb868147dc14fa828532c9bd75
Muhammad Faheem <muhammadfaheem9986@gmail.com>
2026-09-17 11:37:30 +0500

    Add files via upload

---
+++ Assignment_op.py
@@ -0,0 +1,12 @@
+num=50
+num += 30
+
+print("Addig any value to number:",num)
+num -= 10
+print("subtracting any value from number:",num)
+num *= 2
+print("multipling any value with number:",num)
+num /= 3
+print("dividing number by any value:",num)
+num %= 7
+print("any number:",num)
