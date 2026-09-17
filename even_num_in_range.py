a43e1babb23becd2ebc54ecc4cb54cc39051b8c7
Muhammad Faheem <muhammadfaheem9986@gmail.com>
2026-09-17 11:41:18 +0500

    Add files via upload

---
+++ even_num_in_range.py
@@ -0,0 +1,13 @@
+count=1;
+while count<=10:
+    if(count%2==0):
+        print(count)
+    count+=1
+print("Loop ended.")
+# second method to print even number between given range
+count=2;
+while count<=10:
+    print(count)
+    count+=2
+print("Loop ended.")
+
