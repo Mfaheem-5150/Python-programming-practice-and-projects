776929394f26751a60ca7dcc5ef71a3bb3bc558e
Muhammad Faheem <muhammadfaheem9986@gmail.com>
2026-09-17 11:37:07 +0500

    Add files via upload

---
+++ Arithmatic_operator.py
@@ -0,0 +1,12 @@
+# we can either hard code value or take input from user
+# num1=30
+# num2=20
+num1 = float(input("Enter first number:"))
+num2 = float(input("Enter second number:"))
+
+print("Sum of numbers:",num1+num2)
+print("Product of numbers:",num1*num2)
+print("Difference of numbers:",num1-num2)
+print("division of two numbers:",num1/num2)
+ # in python modulus operator works on both data types either interger or float
+print("Modulus:",num1%num2)
