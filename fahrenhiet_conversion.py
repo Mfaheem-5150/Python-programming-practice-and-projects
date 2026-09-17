c3fd48631c4a3cffe5c03f45550df667f480645b
Muhammad Faheem <muhammadfaheem9986@gmail.com>
2026-09-17 12:25:01 +0500

    Add files via upload

---
+++ fahrenhiet_conversion.py
@@ -0,0 +1,6 @@
+# convert fahrenhiet temperature into celsius
+
+F=float(input("Enter temperature in Fahrenhiet : "))
+# F=float((C*9/5)+32) # type conversion in not compusolry here 
+C=(F-32)*5/9
+print("Temperature in Celsius : ",C)
