0525ae4e3ed05441cd4ed0db59bb6748ebf42382
Muhammad Faheem <muhammadfaheem9986@gmail.com>
2026-09-17 11:36:47 +0500

    Add files via upload

---
+++ area_of_rectangle.py
@@ -0,0 +1,19 @@
+length=float(input("enter the length of rectangle:"))
+width=float(input("Enter the width of rectangle:"))
+area=length*width
+print("Area of Rectangle:",area)
+
+#perimeter of rectangle.
+#  first method
+
+print("Perimeter of rectangle is :",2*(length+width))
+
+#second method
+
+perimeter=2*(length+width)
+print("Perimeter of rectangle is :",perimeter)
+
+# Area of   square
+
+side=int(input("enter the  a side of square:"))
+print("Area of square:",side*side)
