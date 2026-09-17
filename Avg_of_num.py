be8d99a805e7adfa38a97fbd8570a85413a9bc87
Muhammad Faheem <muhammadfaheem9986@gmail.com>
2026-09-17 11:37:50 +0500

    Add files via upload

---
+++ Avg_of_num.py
@@ -0,0 +1,5 @@
+num1 = float(input("Enter first number:"))
+num2 = float(input("Enter first number:"))
+num3 = float(input("Enter first number:"))
+avg=(num1+num2+num3)/3
+print("Average of numbers: ",avg)
