f7f85e52726643e0306b512a4ff7bf432a2a4479
Muhammad Faheem <muhammadfaheem9986@gmail.com>
2026-09-17 12:25:29 +0500

    Add files via upload

---
+++ greatest_of_three.py
@@ -0,0 +1,11 @@
+x = int(input("enter first num: "))
+y = int(input("enter second num: "))
+z= int(input("enter third num: "))
+if(x>y):
+    if(x>z):
+        print("x is greater: ",x)
+elif(y>z):
+        print("y is greater: ",y)
+else:
+     print("z is greater: ",z)
+print("program end")
