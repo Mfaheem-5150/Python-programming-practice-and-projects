1b2b02ed8b993a49ae6aec313a729ee20016ca8f
Muhammad Faheem <muhammadfaheem9986@gmail.com>
2026-09-23 17:27:21 +0500

    Add files via upload

---
+++ print_odd_num.py
@@ -0,0 +1,13 @@
+# count=1
+# while count<=20:
+#     print(count)
+#     count+=2
+# print("Loop ended.")
+
+# second method to print odd number between given range
+
+count=1
+while count<=20:
+    if(count%2 != 0):  #same condition as if(count%2==1)
+        print(count)
+    count+=1
