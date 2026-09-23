fc72c01205a631b1d6b7d6b53b4c718d97384909
Muhammad Faheem <muhammadfaheem9986@gmail.com>
2026-09-23 17:31:35 +0500

    Add files via upload

---
+++ swap_numbers.py
@@ -0,0 +1,11 @@
+num1=int(input("Enter a first number : "))
+num2=int(input("Enter a second number : "))
+print("Before swaping the number.")
+print("num1 = ",num1)
+print("num2 = ",num2)
+x=num1
+num1=num2
+num2=x
+print("After swaping the number.")
+print("num1 = ",num1)
+print("num2 = ",num2)
