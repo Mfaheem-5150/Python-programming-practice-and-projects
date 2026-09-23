521dc6b41a4beea602ede4bfb73904d4a5c06ed0
Muhammad Faheem <muhammadfaheem9986@gmail.com>
2026-09-23 17:30:04 +0500

    Add files via upload

---
+++ string _slicing.py
@@ -0,0 +1,11 @@
+str="welcome to python programming"
+# access substr using string slicing
+print(str[0:10])
+print(str[:15])
+print(str[8:])
+# Negative indexing 
+
+print(str[-1])
+# print(str[-1:-5])
+print(str[-11:])
+print(str[:-15])
