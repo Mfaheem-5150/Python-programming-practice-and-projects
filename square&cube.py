70b85412ce5a4ed89ec596072a29319c20361387
Muhammad Faheem <muhammadfaheem9986@gmail.com>
2026-09-23 17:29:39 +0500

    Add files via upload

---
+++ square&cube.py
@@ -0,0 +1,15 @@
+num=int(input("Enter a number : "))
+# "**" double asteriks represent power operator in python
+sqr=num**2  
+cb=num**3
+print("Square of a number : ",sqr)
+print("Cube of a number : ",cb)
+
+#second method
+'''
+num=int(input("Enter a number : "))
+sqr=num*num  
+cb=num*num*num
+print("Square of a number : ",sqr)
+print("Cube of a number : ",cb)
+'''
