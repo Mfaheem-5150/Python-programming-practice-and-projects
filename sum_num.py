e55c6636abca899e34c548ca66febdbb06d90190
Muhammad Faheem <muhammadfaheem9986@gmail.com>
2026-09-23 17:31:09 +0500

    Add files via upload

---
+++ sum_num.py
@@ -0,0 +1,13 @@
+# x=10
+# y=23.44
+# z=x+y
+# print("Sum of two numbers : ",x+y)
+
+# #second way to print sum
+
+# print("Sum of two numbers : ",z)
+
+# Taking input from the user
+first_num = int(input("Enter a first number:"))
+second_num = float(input("Enter a second number:"))
+print("Sum of numbers is ",first_num + second_num)
