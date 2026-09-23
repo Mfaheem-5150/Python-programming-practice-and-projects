e884a8ccf00b5fb7829034fbf84d10e0d30c7161
Muhammad Faheem <muhammadfaheem9986@gmail.com>
2026-09-23 17:29:06 +0500

    Add files via upload

---
+++ smallest_of_three.py
@@ -0,0 +1,10 @@
+x = int(input("enter first num: "))
+y = int(input("enter second num: "))
+z= int(input("enter third num: "))
+if(x<y and x<z):
+        print("x is smaller: ",x)
+elif(y<z):
+        print("y is smaller: ",y)
+else:
+     print("z is smaller: ",z)
+print("program end")
