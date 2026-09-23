06a0732770814699dd18a2a67e9d43de4c17796b
Muhammad Faheem <muhammadfaheem9986@gmail.com>
2026-09-23 17:30:47 +0500

    Add files via upload

---
+++ string_operations.py
@@ -0,0 +1,23 @@
+str="Hello"
+str1="world"
+str2="welcome to python programming"
+#string concatenation operation to join strings
+print(str+str1)
+final_str=str+ " "+str1+ " " +str2
+print(final_str)
+#second method
+print(str+ " "+str1+ " " +str2)
+#finding the length of string using "len()" function  
+print(len(str))
+print(len(str1))
+print(len(str2))
+#accesing particular character of string
+
+str="Govt Graduate college"
+print(str[0])
+print(str[1])
+print(str[5])
+
+# in string type we can only acces not manipulate the particular index
+# str[0]='A'
+# print(str[0])
