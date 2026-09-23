1a0aba9c4a3a3d5b48130c721125f6684f398148
Muhammad Faheem <muhammadfaheem9986@gmail.com>
2026-09-23 17:32:35 +0500

    Add files via upload

---
+++ type_casting.py
@@ -0,0 +1,12 @@
+num1 = "9"
+num2 = 4.55
+#print("sum = ",num1+num2)#here an error occure due to adding str type to float so the type casting is required
+
+num1 = int(num1)
+print("sum = ",num1+num2)
+
+'''here the error occure due to large number
+that is not convertable to string type and vice versa'''
+#x=int("faheem")
+#print(x)
+# num2=str(num2) 
